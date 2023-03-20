e993acf04ac7c6291615484b83d851460b675d12
Chu Seng <yongchuseng@gmail.com>
2023-03-20 16:46:55 +0800

    First commit.

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+
+function Header() {
+  return (
+    <div>
+          This is amazon header
+          <SearchIcon />
+          <ShoppingBasketIcon />
+    </div>
+  );
+}
+
+export default Header
---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../Components/Header";
+
+function Home() {
+  return <div className='home'>
+    <Header />
+    This is Home Page
+  </div>;
+}
+
+export default Home;
