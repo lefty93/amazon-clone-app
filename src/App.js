620ee5cbf8093362eafaebc210634d542f26d071
Lefty93 <yongchuseng@gmail.com>
2023-03-27 17:42:52 +0800

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Checkout from "./Pages/Checkout";
+import Login from "./Pages/Login";
 import Header from "../src/Components/Header"
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/checkout' element={<Checkout />}></Route>
           <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
         </Routes>
       </Router>
     </div>
