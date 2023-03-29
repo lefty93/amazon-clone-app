70c1aa634cc1570d8d40cd4f977cee3d43e3913b
Chu Seng <yongchuseng@gmail.com>
2023-03-29 15:26:02 +0800

    Deploy on firebase

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,40 +8,48 @@ import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 
+import { getAuth, signOut } from 'firebase/auth';
+
 
 
 function Header() {
   // const [{ basket }, dispatch] = useContext(StateContext)
-  const [{basket}, dispatch] = useStateValue()
-  console.log(basket)
+  const [{ basket, user }, dispatch] = useStateValue()
+
+  const auth = getAuth()
+  const login = () => {
+    
+    signOut(auth)
+
+  }
   return (
     <div className="header">
       <Link to='/'>
         <img src="https://amazon-blogs-brightspot-lower.s3.amazonaws.com/about/a9/af/27a4ef844ac38129d0fa460675fb/amazon-logo.svg" alt="" className="header__logo" />
       </Link>
-      
+
       <div className="header__location">
         <LocationOnOutlinedIcon />
         <div className="header__optionLocation">
           <span className="header__optionLineOne">Deliver to</span>
           <span className="header__optionLineTwo">Malaysia</span>
         </div>
-        
+
       </div>
 
       <div className="header__search">
-        <input type="text" className="header__searchInput" placeholder='Search'/>
+        <input type="text" className="header__searchInput" placeholder='Search' />
         <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
-        
-        <div className="header__option">
-          <span className="header__optionLineOne">Hello Guest</span>
-          <Link to='/login' className="header__optionLineTwo header__loginOption">
-            <span>Sign In</span>
-          </Link>
-        </div>
+        <Link to={!user && '/login'} className="header__loginOption">
+          <div onClick={login} className="header__option">
+            <span className="header__optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
+            <span className="header__optionLineTwo">{user ? 'Sign out' : 'Sign in'}</span>
+          </div>
+        </Link>
+
 
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
@@ -52,16 +60,16 @@ function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
-        
+
         <div className="header__optionBasket">
           <Link to='/checkout' className="header__basketIcon">
             <ShoppingCartOutlinedIcon />
           </Link>
           <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
         </div>
-        
+
       </div>
-    </div>
+    </div >
   );
 }
 
